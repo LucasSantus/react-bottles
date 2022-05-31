b748b61d6dd80472552c5fd5f0250419dec51a8e
Lucas Santos <leos9877@gmail.com>
2022-05-31 09:20:07 -0300

    :tada: add new project

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,10 @@
+
+export default function Dashboard() {
+    return (
+        <div className="bg-slate-900 flex flex-col justify-center items-center w-full h-screen">
+            <h1 className="text-purple-900 underline">
+                Dashboard
+            </h1>
+        </div>
+    )
+}
