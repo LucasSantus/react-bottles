da5d808f014c61830ef0f3c11eaa3d475ca825d4
Lucas Santos <leos9877@gmail.com>
2022-05-31 13:57:38 -0300

    :pushpin: improving sidebar menu codes.

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,21 +3,16 @@ import { useState } from "react";
 export default function Sidebar() {
     const [open, setOpen] = useState(true);
 
-    const Menus = [
+    const menu_items = [
         { title: "Dashboard", src: "Chart_fill" },
-        { title: "Inbox", src: "Chat" },
-        { title: "Accounts", src: "User", gap: true },
-        { title: "Schedule ", src: "Calendar" },
-        { title: "Search", src: "Search" },
-        { title: "Analytics", src: "Chart" },
-        { title: "Files ", src: "Folder", gap: true },
-        { title: "Setting", src: "Setting" },
+        // { title: "Inbox", src: "Chat" },
+        // { title: "Accounts", src: "User", gap: true },
     ];
 
     return (
         <div
-            className={` ${
-            open ? "w-72" : "w-20"
+            className={`${
+                open ? "w-72" : "w-20"
             } bg-slate-900 h-screen p-5  pt-8 relative duration-300`}
         >
             <img
@@ -37,24 +32,21 @@ export default function Sidebar() {
                 />
                 <h1
                     className={`text-white origin-left font-medium text-xl duration-200 ${
-                    !open && "scale-0"
+                        !open && "scale-0"
                     }`}
                 >
-                    Designer
+                    Adega UBUNTU
                 </h1>
             </div>
             <ul className="pt-6">
-                {Menus.map((Menu, index) => (
+                {menu_items.map((item, index) => (
                     <li
                         key={index}
-                        className={`flex  rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4 
-                        ${Menu.gap ? "mt-9" : "mt-2"} ${
-                            index === 0 && "bg-light-white"
-                        } `}
+                        className={`flex  rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4`}
                     >
-                        <img src={`./src/assets/${Menu.src}.png`} />
+                        <img src={`./src/assets/${item.src}.png`} />
                         <span className={`${!open && "hidden"} origin-left duration-200`}>
-                            {Menu.title}
+                            {item.title}
                         </span>
                     </li>
                 ))}
